5ed1249103d1606787359ba92076433c200e096a
Oswaldo Hernandez <20191155@uthh.edu.mx>
2022-10-07 11:08:05 -0500

    cambio1

--- src/data/repositories.js
+++ src/data/repositories.js
@@ -1,5 +1,13 @@
 export default[
     {
-        id:'midudev.mid'
+        id:'midudev.mid',
+        fullName: 'django/django',
+        description: 'The web framework for perfectionists with deadLines.',
+        language: 'Python',
+        forksCount:'21015',
+        stargazersCount:48496,
+        ratingAverage:73,
+        reviewCount:5,
+
     }
 ]
